5fd0bf30f4d2e6e65886f14dc372cca6b80a2449
Gurdeep Singh <gurdeepmundi10@gmail.com>
2022-03-16 02:48:31 -0600

    better solution for 217🏎

--- leetcode exercises/217_contains_duplicates.cs
+++ leetcode exercises/217_contains_duplicates.cs
@@ -1,13 +1,15 @@
 // this solution is in c#
+using System.Collections.Generic;
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        for(int i = 0; i < nums.Length -1; i++)
-            {
-                for(int j = i+1; j < nums.Length; j++)
-                {
-                    if (nums[i] == nums[j]) return true;
-                }
-            }
+        Dictionary<int, bool> occassions = new Dictionary<int, bool>(); 
+        for(int i = 0; i < nums.Length; i++) {
+            if(occassions.ContainsKey(nums[i])) {
+                return true;
+            } else {
+                occassions.Add(nums[i], false);
+            }   
+        }
         return false;
     }
 }
