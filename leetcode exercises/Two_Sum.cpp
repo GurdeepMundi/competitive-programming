d8520c2c75c7788c125cabdc81dec44b2dfccdc5
Gurdeep Singh <gurdeepmundi10@gmail.com>
2022-04-12 23:21:03 -0600

    reverting to this state to bring back lost files in leetcode exercises folder

--- leetcode exercises/Two_Sum.cpp
+++ leetcode exercises/Two_Sum.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int size = nums.size();
-        vector <int> out;
-        for (int i=0; i< size-1; i++) {
-            for(int j= i+1; j < size; j++) {
-                if(nums.at(i) + nums.at(j) == target) { 
-                out.push_back(i);
-                out.push_back(j);
-                }
+        unordered_map<int, int> a;
+        unordered_map<int, int>::iterator it;
+        vector<int> out(2,0);
+        for(int i = 0; i < nums.size(); i++) {
+            it = a.find(target-nums[i]);
+            if(it != a.end()) {
+                out[0] = it->second;
+                out[1] = i;
+            }else {
+                a[nums[i]] = i;
             }
         }
-          return out;  
-        }
+        return out;
+    }
 };
