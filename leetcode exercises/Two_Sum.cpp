1473de19339bd991900ce49ee6931199ecc8bb17
Gurdeep Singh <gurdeepmundi10@gmail.com>
2022-04-12 23:14:27 -0600

    Revert "🏎 O(n) time complexity solution for Two sum"
    This reverts commit 58493acea32d238d1deee1ad8e012f765bcfb046.

--- leetcode exercises/Two_Sum.cpp
+++ leetcode exercises/Two_Sum.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> a;
-        unordered_map<int, int>::iterator it;
-        vector<int> out(2,0);
-        for(int i = 0; i < nums.size(); i++) {
-            it = a.find(target-nums[i]);
-            if(it != a.end()) {
-                out[0] = it->second;
-                out[1] = i;
-            }else {
-                a[nums[i]] = i;
+        int size = nums.size();
+        vector <int> out;
+        for (int i=0; i< size-1; i++) {
+            for(int j= i+1; j < size; j++) {
+                if(nums.at(i) + nums.at(j) == target) { 
+                out.push_back(i);
+                out.push_back(j);
+                }
             }
         }
-        return out;
-    }
+          return out;  
+        }
 };
